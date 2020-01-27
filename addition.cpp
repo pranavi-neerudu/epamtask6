a8f567505e38c36a994a7af855547a5adbc5e9af
pranavi-neerudu <saipranavireddyneerudu@gmail.com>
2020-01-27 21:04:40 +0530

    first commit

---
+++ addition.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"SUM OF TWO NUMBERS"<<endl;
+    cout<<"ENTER TWO NUMBERS"<<endl;
+    cin>>a>>b;
+    cout<<"SUM OF TWO NUMBERS"<<endl;
+    cout<<a+b<<endl;
+
+
+    return 0;
+}
