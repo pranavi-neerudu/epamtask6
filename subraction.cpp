479d168254de8c114598da539cc3cce3929696f7
pranavi-neerudu <saipranavireddyneerudu@gmail.com>
2020-01-27 21:28:31 +0530

    first commit

---
+++ subraction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"SUM OF TWO NUMBERS"<<endl;
+    cout<<"ENTER TWO NUMBERS"<<endl;
+    cin>>a>>b;
+    cout<<"Difference OF TWO NUMBERS"<<endl;
+    cout<<a+b<<endl;
+
+
+    return 0;
+}
+
