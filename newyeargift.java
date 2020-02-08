0a35d7228a100af4ea7a3344e38907bf448709de
pranavi-neerudu <saipranavireddyneerudu@gmail.com>
2020-02-08 21:29:53 +0530

    firstcommit

---
+++ newyeargift.java
@@ -0,0 +1,46 @@
+import java.io.*
+import java.util.*
+class Newyeargift{
+    public class Sweets
+    {
+        int x;
+        Sweets(int a)
+        {
+            this.x=a;
+
+        }
+
+
+    }
+    public class Candy
+    {
+        int x;
+        Candy(int a)
+        {
+            this.x=a;
+
+        }
+
+
+    }
+    public class Choco
+    {
+        int x;
+        Choco(int a)
+        {
+            this.x=a;
+
+        }
+
+
+    }
+    public static void main(String[] args)
+    {
+
+     Sweets s=new Sweets(5);
+     Choco c=new Choco(1);
+     Candy ca=new Candy(10);
+     System.out.println(5+1+10);
+
+    }
+}
